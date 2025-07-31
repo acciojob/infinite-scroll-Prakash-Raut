81cfde16e434a6ae28719e5060257cc4747ddf75
Prakash Raut <prakash_raut@outlook.com>
2025-07-31 10:08:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 //your code here!
+const ele = document.getElementById("infi-list");
 
+let count = 1;
+
+function addItems(num) {
+	for (let i = 0; i < num; i++) {
+		const li = document.createElement('li');
+		li.innerText = `Item ${count}`;
+		ele.appendChild(li);
+		count++;
+	}
+}
+
+addItems(10);
+
+ele.addEventListener('scroll', () => {
+	if(ele.scrollTop + ele.clientHeight >= ele.scrollHeight - 1) {
+		addItems(2);
+	}
+})
